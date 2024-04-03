9722294ead7d5ead53d3ccfed7e256626ac4619d
Manas Sachdeva <113659274+manassachdeva@users.noreply.github.com>
2024-04-03 16:44:53 +0000

    container config done

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we r using nodejs ; not browser
+
+console.log("manas")
+
+let name="manas"
+let age=19
+let islOGGED=false
+
+//number => 2 power 53
+// bigint
+//string=""
+//boolean=>true/false
+//null =>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof "manas")//string
+console.log(typeof null) //object
+console.log(typeof undefined)//undefined
