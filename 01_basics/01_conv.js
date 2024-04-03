1eb4736831382e7d2dc8fdcc1ff570ab174eeed5
Manas Sachdeva <113659274+manassachdeva@users.noreply.github.com>
2024-04-03 17:00:09 +0000

    container config done

---
+++ 01_basics/01_conv.js
@@ -0,0 +1,15 @@
+let number=33
+console.log(typeof number)
+let n2="33"
+let stringtonumber=Number(n2)
+console.log(typeof stringtonumber)
+//"33"=>33
+//"33abc"=>NaN
+// true=>1
+//false=>0
+let isloo=1
+let booleanss=Boolean(isloo)
+console.log(booleanss)
+//1=true,0=false
+//""=false
+//manas=true
