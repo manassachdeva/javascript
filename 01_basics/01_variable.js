3c5b6fd3881a9b4ed45305b0d17096612c92c9c3
Manas Sachdeva <113659274+manassachdeva@users.noreply.github.com>
2024-04-03 16:20:10 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="manas@gmail.com"
+var accpass="12wsd"
+acccity='kanpur'
+
+// accountId=2        # we can not change const variable
+console.log(accountId)
+
+/*Prefer not to use var
+beacuse of functional scope or block scope
+*/
+
+console.table([accountId,accountEmail,accpass,acccity])
