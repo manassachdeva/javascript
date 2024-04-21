34b70dcda720536e74d7068c4ebed874eabcf77d
Manas Sachdeva <113659274+manassachdeva@users.noreply.github.com>
2024-04-21 05:55:59 +0000

    container config done

---
+++ 01_basics/07_math.js
@@ -0,0 +1,15 @@
+const score=400;
+console.log(score)
+const balance =new Number(100)
+console.log(balance)
+
+const othernumber=23.98
+console.log(othernumber.toPrecision(2))
+
+const hundreds=1000000;
+console.log(hundreds.toLocaleString());
+
+console.log(Math.random());
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
